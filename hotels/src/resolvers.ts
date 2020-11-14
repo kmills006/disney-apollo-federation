213f549ce33ec3091f22dca65c26f3f0d733b0cd
Kristy Miller <kristy@goodmoney.com>
2020-11-14 12:37:33 -0800

    refactor: clean up all old packages

--- hotels/src/resolvers.ts
+++
@@ -1,9 +0,0 @@
-export const resolvers = {
-  Query: {
-    hotels: async (obj: undefined, args: undefined, context: any) => {
-      const { dataSources } = context;
-
-      return dataSources.hotelsApi.getHotels();
-    },
-  },
-};
