0fe1ed8f29a87b0b58f65d9f463119a818aacbf0
Kristy Miller <kristy@goodmoney.com>
2020-11-24 13:25:19 -0800

    feat(parks): test getParkByPermalink

--- parks/src/__tests__/repositories/parks.test.ts
+++ parks/src/__tests__/repositories/parks.test.ts
@@ -1,14 +1,34 @@
+import * as O from 'fp-ts/lib/Option';
+
 import { MOCK_PARKS } from '../../__mocks__/mockParks';
 import { repository } from '../../repositories/parks';
 
 describe('respositories', () => {
   describe('parks', () => {
     describe('getParks', () => {
-      it('returns right with a list of parks', () => {
+      it('returns a list of parks', () => {
         const systemUnderTest = repository(MOCK_PARKS)();
         const actual = systemUnderTest.getParks();
 
         expect(actual).toStrictEqual(MOCK_PARKS);
+      });
+    });
+
+    describe('getParkByPermalink', () => {
+      const mockPark = MOCK_PARKS[Math.floor(Math.random() * MOCK_PARKS.length)];
+
+      it('returns right with a park by its permalink', () => {
+        const systemUnderTest = repository(MOCK_PARKS)();
+        const actual = systemUnderTest.getParkByPermalink(mockPark.permalink);
+
+        expect(O.isSome(actual)).toBeTruthy();
+      });
+
+      it('returns left when permalink doesnt match a park', () => {
+        const systemUnderTest = repository(MOCK_PARKS)();
+        const actual = systemUnderTest.getParkByPermalink('test-permalink');
+
+        expect(O.isSome(actual)).toBeFalsy();
       });
     });
   });
