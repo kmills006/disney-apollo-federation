0dc684bbb34f4d03a54378e685b7cf4d7a9de010
Kristy Miller <kristy@goodmoney.com>
2020-11-24 15:35:20 -0800

    feat(gql-utils): migrate construct and start

--- packages/gql-utils/src/index.ts
+++ packages/gql-utils/src/index.ts
@@ -1,1 +1,26 @@
-console.log('hi');
+import { ApolloServer } from 'apollo-server';
+import { GraphQLSchema } from 'graphql';
+
+export interface IApolloServerConfiguration<C> {
+  schema: GraphQLSchema;
+  context: () => C;
+  engine?: boolean;
+}
+
+export const constructApolloServer = <C = {}>(
+  config: IApolloServerConfiguration<C>,
+): ApolloServer => (
+    new ApolloServer({
+      schema: config.schema,
+      context: () => config.context(),
+      engine: config.engine || false,
+    })
+  );
+
+export const startApolloServer = (
+  port: number,
+) => async (s: ApolloServer): Promise<void> => {
+  s.listen(port).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+};
