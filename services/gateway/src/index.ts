280b86d003f38132e1f8c5bb932c5232f43203fe
Kristy Miller <kristy@goodmoney.com>
2020-11-30 15:04:21 -0800

    feat(gateway): setup basic gateway with local managed service list

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello from the gateway');
+import { ApolloGateway } from '@apollo/gateway';
+import { ApolloServer } from 'apollo-server';
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'parks', url: 'http://localhost:4000' },
+    { name: 'attractions', url: 'http://localhost:4001' },
+  ],
+});
+
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+server.listen({ port: 3000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
