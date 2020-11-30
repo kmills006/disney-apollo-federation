6774cfb91ab0f983177c1df209a6c5a379f5145e
Kristy Miller <kristy@goodmoney.com>
2020-11-30 14:38:23 -0800

    feat(gql-utils): move shared types to utils

--- packages/gql-utils/src/index.ts
+++ packages/gql-utils/src/index.ts
@@ -1,8 +1,17 @@
 import { ApolloServer } from 'apollo-server';
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, GraphQLResolveInfo } from 'graphql';
 import { applyMiddleware } from 'graphql-middleware';
 
 import { logResolverTime } from './middleware';
+
+export type ResolveType<P, T> = (parent: P) => T | null;
+
+export type ResolverFunc<R, C = {}, A = {}, P = {}> = (
+  parent: P,
+  args: A,
+  ctx: C,
+  info: GraphQLResolveInfo
+) => R;
 
 export interface IApolloServerConfiguration<C> {
   schema: GraphQLSchema;
@@ -26,4 +35,13 @@ export const startApolloServer = (
   s.listen(port).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
   });
+};
+
+export const findTypeNameInParent = <T extends string, P = {}>(
+  parent: P,
+  mapping: string[][],
+): T | null => {
+  const [, typename] = mapping.find(([property]) => property in parent) || [null, null];
+
+  return typename as T;
 };
