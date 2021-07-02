0403f016182f247354c25154bde507f6857dd547
Kristy Miller <kristy@goodmoney.com>
2021-07-02 16:08:38 -0700

    refactor: remove out and outdated project

--- src/config.ts
+++
@@ -1,4 +0,0 @@
-export const ATTRACTIONS_SERVICE = 'http://localhost:4001/graphql';
-export const DINING_SERVICE = 'http://localhost:4002/graphql';
-export const HOTELS_SERVICE = 'http://localhost:4003/graphql';
-export const PARKS_SERVICE = 'http://localhost:4004/graphql';
