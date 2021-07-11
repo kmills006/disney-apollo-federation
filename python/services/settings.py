4262be0cb57e8af1fcec9750ac3047251f1df5d0
Kristy Miller <kristy@goodmoney.com>
2021-07-10 18:15:47 -0700

    fix(python): pull secret from os

--- python/services/settings.py
+++ python/services/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-th^@r56h@j)$ypjv&-0213t0uiiek55@amy1g!=)2de1gi*0ox'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
